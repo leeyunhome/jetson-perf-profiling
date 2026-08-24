227c72509b0e082b54fb9d362a5ce0bc512f1f3f
manager <mszeta@naver.com>
2026-08-24 21:44:28 +0900

    style: add .clang-format, force multi-line function bodies

--- src/hot.c
+++ src/hot.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
-long fib(int n) { return n < 2 ? n : fib(n - 1) + fib(n - 2); }
+long fib(int n) {
+  return n < 2 ? n : fib(n - 1) + fib(n - 2);
+}
 int main() {
   printf("%ld\n", fib(42));
   return 0;
