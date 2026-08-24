2096e923bf8159a85ecba231595d2c21bd95da8a
manager <mszeta@naver.com>
2026-08-24 21:42:15 +0900

    style: format hot.c with clang-format

--- src/hot.c
+++ src/hot.c
@@ -1,3 +1,6 @@
 #include <stdio.h>
-long fib(int n){ return n<2 ? n : fib(n-1)+fib(n-2); }
-int main(){ printf("%ld\n", fib(42)); return 0; }
+long fib(int n) { return n < 2 ? n : fib(n - 1) + fib(n - 2); }
+int main() {
+  printf("%ld\n", fib(42));
+  return 0;
+}
